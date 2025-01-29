ea9334b54d97e233d1de2207f02125b94a314b99
suhailvs <suhailvs@gmail.com>
2025-01-29 14:02:48 +0530

    updated main

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ source env/bin/activate
 pip install -r requirement.txt
 python main.py
 ```
+
+### deepseek
+
+download the below model and use it
+https://huggingface.co/TheBloke/deepseek-coder-6.7B-instruct-GGUF/blob/main/deepseek-coder-6.7b-instruct.Q3_K_S.gguf
--- main.py
+++ main.py
@@ -2,16 +2,41 @@
 from gpt4all import GPT4All
 import sys
 
+# https://huggingface.co/TheBloke
+# https://huggingface.co/TheBloke/deepseek-coder-6.7B-instruct-GGUF
+models = {
+    'orca-mini':'orca-mini-3b-gguf2-q4_0.gguf',
+    'deepseek-llm-small':'deepseek-llm-7b-chat.Q3_K_S.gguf', # https://huggingface.co/TheBloke/deepseek-llm-7B-chat-GGUF/blob/main/deepseek-llm-7b-chat.Q3_K_S.gguf
+    'deepseek-coder-small':'deepseek-coder-6.7b-instruct.Q3_K_S.gguf',
+    'deepseek-coder-medium':'deepseek-coder-6.7b-instruct.Q5_K_M.gguf',
+}
+    
+    
+    
+inputs = [
+    "How many overs are there is in a one day cricket match?",
+    "How to calculate fibonnaci series in python?",
+]
 
-def draw_cat():
-    model = GPT4All(model_name='orca-mini-3b-gguf2-q4_0.gguf', allow_download=False, model_path='.')
-    tokens = []
-    for token in model.generate("How many overs are there is in a one day cricket match?", streaming=True):
-        tokens.append(token)
-        sys.stdout.write(token)
+# def draw_cat():
+#     model = GPT4All(model_name=models[1], allow_download=False, model_path='.')
+#     tokens = []
+#     for token in model.generate(inputs[1], streaming=True):
+#         tokens.append(token)
+#         sys.stdout.write(token)
 
-    sys.stdout.flush()
+#     sys.stdout.flush()
 
+
+
+def example():
+    model = GPT4All(model_name=models['deepseek-llm-small'], allow_download=False, model_path='.')
+    with model.chat_session():
+        for i in range(3):
+            t = input('>> ')
+            if t=='q': break
+            print(model.generate(t))
 
 if __name__ == '__main__':
-    draw_cat()
+    # draw_cat()
+    example()
